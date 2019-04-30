a1ddb631a9a220f87d4b375bfe8805d25289d141
junjiechen1 <37597962+junjiechen1@users.noreply.github.com>
2019-04-30 11:57:10 -0400

    added icon library

--- pmp4_demo_project/android/app/src/main/java/com/pmp4_demo_project/MainApplication.java
+++ pmp4_demo_project/android/app/src/main/java/com/pmp4_demo_project/MainApplication.java
@@ -3,11 +3,11 @@ package com.pmp4_demo_project;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,6 +23,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage()
+          , new VectorIconsPackage()
       );
     }
 
--- pmp4_demo_project/index.js
+++ pmp4_demo_project/index.js
@@ -1,23 +1,14 @@
-/**
- * @format
- */
-import React from'react';
-import {AppRegistry} from 'react-native';
-import{Provider} from'react-redux';
-
-import App from './App';
-import {name as appName} from './app.json';
 import React from 'react';
 import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
 import App from './App';
-import configureStore from'./src/store/configureStore';
-
+import configureStore from './src/store/configureStore';
 const store = configureStore();
 
 const RNRedux =()=>(
     <Provider store = {store}>
         <App />
     </Provider>
-)
-AppRegistry.registerComponent(appName, () => RNRedux);
+);
+
+AppRegistry.registerComponent('pmp4_demo_project', () => RNRedux);
